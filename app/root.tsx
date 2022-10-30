eced2e310274ccbbdc4e220a55fd18666880f33f
vonrie <vonrie@lanturn.com>
2022-10-30 19:51:37 +0800

    init

---
+++ app/root.tsx
@@ -0,0 +1,62 @@
+import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useActionData,
+  useLoaderData,
+} from "@remix-run/react";
+
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
+import { getEnv } from "./env.server";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Remix Notes",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+  ENV: ReturnType<typeof getEnv>;
+};
+
+export async function loader({ request }: LoaderArgs) {
+  console.log("env", getEnv());
+  return json<LoaderData>({
+    user: await getUser(request),
+    ENV: getEnv(),
+  });
+}
+
+export default function App() {
+  const { ENV } = useLoaderData() as LoaderData;
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(ENV)}`,
+          }}
+        />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
